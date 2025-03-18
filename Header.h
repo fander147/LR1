77b542e98bec227ebe8d7d50ff6ebda1c46460a4
fander147 <petya.shchepilov@gmail.com>
2025-03-18 20:38:55 +0300

    Update Header.h

--- Header.h
+++ Header.h
@@ -5,5 +5,8 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <limits>
 
 #define CLEARLINE(n) for (int i=0;i<n;i++) cout << "\033[F\r\033[K"
+#define LINES(a) for (int i=0;i < a;i++) cout << endl
+#define CURSORPOS GetCursorPosition();
