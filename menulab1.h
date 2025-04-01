70a247bec8a1b82a6a1ce9fbab60d466d726d860
fander147 <petya.shchepilov@gmail.com>
2025-04-01 20:54:09 +0300

    Add files via upload

---
+++ menulab1.h
@@ -0,0 +1,16 @@
+#pragma once
+int sizeMas();
+void save();
+void load();
+void inmas();
+void outmas(double masi[]);
+void lab1res();
+
+void lab1Dot0();
+void lab1Dot1();
+void lab1Dot2();
+void lab1Dot3();
+void lab1Dot4();
+void lab1Dot5();
+void lab1Dot6();
+void lab1Dot7();
