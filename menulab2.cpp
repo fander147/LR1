b806acf6d9ed038ce79872cf6d1c9f1a0b942278
fander147 <petya.shchepilov@gmail.com>
2025-04-01 22:28:33 +0300

    Update menulab2.cpp

--- menulab2.cpp
+++ menulab2.cpp
@@ -7,8 +7,23 @@ using namespace std;
 void Table(double* pmas, int pnsize, int pnsec, string pstitle);
 void Tabletex(double* mas, int N, const string& title, const string& namep);
 
+int WriteBinaryFile(std::string psfilename, bool pisappend = false);
+int ReadBinaryFile(std::string);
+
+void lab2Dot1();
+void lab2Dot2();
+void lab2Dot3();
+int nres = 0;
 void menulab2() {
+	masA = new double[N];
+	masB = new double[N];
+	for (int i = 0; i < N; i++) {
+		masA[i] = masB[i] = 0;
+	}
+
 	char ch = 0;
+	
+	
 	do {
 
 		system("cls");
@@ -32,9 +47,9 @@ void menulab2() {
 		case '0': lab1Dot0(); break;
 		case '1': lab1Dot1(); break;
 		case '2': lab1Dot2(); break;
-		case '3': lab1Dot3(); break;
-		case '4': lab1Dot4(); break;
-		case '5': lab1Dot5(); break;
+		case '3': lab2Dot1(); break;
+		case '4': lab2Dot2(); break;
+		case '5': lab2Dot3(); break;
 		case '6': lab1Dot6(); break;
 		case '7': lab1Dot7(); break;
 
@@ -45,6 +60,61 @@ void menulab2() {
 		}
 
 	} while (ch != 27);
+}
+void lab2Dot1() {
+	nres = WriteBinaryFile("l.txt");
+	cout << nres;
+	system("pause");
+}
+void lab2Dot2() {
+
+}
+void lab2Dot3() {
+
+}
+
+
+int WriteBinaryFile(string psfilename, bool pisappend) {
+	if (psfilename.empty()) { return -1; }
+	if (masA == 0) return -2;
+
+	fstream fout;
+	if (pisappend) {
+		fout.open(psfilename, ios::binary | ios::app);
+		fout.seekp(0, ios::end);
+
+	}
+	else
+		fout.open(psfilename, ios::binary | ios::out);
+
+	for (int i = 0; i < N; i++) {
+		fout.write((char*)masA + i, sizeof(double));
+	}
+	fout.close();
+	return 0;
+}
+
+int ReadBinaryFile(string psfilename) {
+	if (psfilename.empty()) return -1;
+
+	ifstream fin(psfilename, ios::binary);
+	if (!(fin.bad() , fin.fail())) {
+		//сколько данных в файле
+		fin.seekg(0, ios::end);
+		N = fin.tellg() / sizeof(double);
+
+		if (masA != NULL) delete[] masA;
+		masA = new double [N] {};
+
+		int n = 0;
+		fin.seekg(0);
+		while (!fin.eof() && n < N) {
+			fin.read((char*)(masA + n), sizeof(double));
+			n++;
+		}
+	}
+	fin.close();
+	return 0;
 }
 
 
